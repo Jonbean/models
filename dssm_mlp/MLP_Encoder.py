a6f2727bb30e0d9ca281e4043fd3bb7a2ff98463
Jon <jonczbean@gmail.com>
2016-08-09 23:39:06 -0500

    add MLP_Encoder

---
+++ dssm_mlp/MLP_Encoder.py
@@ -0,0 +1,49 @@
+import theano
+import theano.tensor as T
+import lasagne
+
+
+
+class MLPEncoder(object):
+    def __init__(self, LAYER_UNITS):
+        self.layer1_units = LAYER_UNITS[0]
+        self.layer2_units = None
+        self.layer3_units = None
+        if len(LAYER_UNITS) > 1:
+            self.layer2_units = LAYER_UNITS[1]
+        if len(LAYER_UNITS) > 2:
+            self.layer3_units = LAYER_UNITS[2]
+        self.l_in = None
+        self.output = None
+
+    def build_model(self, input_dim):
+
+        #create symbolic representation of inputs, mask and target_value
+
+        # l_in input shape ==> (n_batch, n_time_steps, n_features)
+        # The number of feature dimensions is 1(index). 
+        self.l_in = lasagne.layers.InputLayer(shape=(None, input_dim))
+
+
+        l_hid1 = lasagne.layers.DenseLayer(self.l_in, num_units=self.layer1_units,
+                                            nonlinearity=lasagne.nonlinearities.tanh,
+                                            W=lasagne.init.GlorotUniform())
+        if self.layer3_units != None:
+            l_hid2 = lasagne.layers.DenseLayer(l_hid1, num_units=self.layer2_units,
+                                            nonlinearity=lasagne.nonlinearities.tanh,
+                                            W=lasagne.init.GlorotUniform())
+            l_hid3 = lasagne.layers.DenseLayer(l_hid2, num_units=self.layer3_units,
+                                            nonlinearity=lasagne.nonlinearities.tanh,
+                                            W=lasagne.init.GlorotUniform())
+            self.output = l_hid3
+            self.all_params = lasagne.layers.get_all_params(l_hid3)
+        elif self.layer3_units == None and self.layer2_units != None:
+            l_hid2 = lasagne.layers.DenseLayer(l_hid1, num_units=self.layer2_units,
+                                            nonlinearity=lasagne.nonlinearities.tanh,
+                                            W=lasagne.init.GlorotUniform())
+
+            self.output = l_hid2
+            self.all_params = lasagne.layers.get_all_params(l_hid2)
+        else:
+            self.output = l_hid1
+            self.all_params = lasagne.layers.get_all_params(l_hid1)
