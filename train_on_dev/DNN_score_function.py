8aaaa25a8c882ea4ce3576f5760ee67589737eb5
Zheng Cai <jontsai@jontsai.ttic.edu>
2017-01-19 19:18:03 -0600

    add dropout

--- train_on_dev/DNN_score_function.py
+++ train_on_dev/DNN_score_function.py
@@ -7,16 +7,18 @@ class DNN(object):
     def __init__(self, 
                  INPUTS_SIZE, 
                  LAYER_UNITS, 
-                 final_nonlin = lasagne.nonlinearities.tanh):
+                 final_nonlin = lasagne.nonlinearities.tanh,
+                 dropout_rate = 0.2):
 
         self.inputs_size = INPUTS_SIZE
         self.layer_units = LAYER_UNITS
         self.final_nonlin = final_nonlin
         self.hidden_layers = []
         self.dropout_rate = 0.0
-
+        
         self.l_in = lasagne.layers.InputLayer(shape=(None, self.inputs_size))
         self.hidden_layers.append(self.l_in)
+        self.hidden_layers.append(lasagne.layers.DropoutLayer(self.hidden_layers[-1], p = dropout_rate))
         for i in range(len(self.layer_units)-1):
             self.hidden_layers.append(lasagne.layers.DenseLayer(self.hidden_layers[i], 
                                         num_units=self.layer_units[i],
